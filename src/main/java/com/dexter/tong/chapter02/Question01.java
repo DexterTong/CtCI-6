3478478aeb6762b27891aa08c7e584c0c3f71999
Dexter Tong <dyt217@nyu.edu>
2018-05-26 17:15:59 -0400

    added ch2q1

--- src/main/java/com/dexter/tong/chapter02/Question01.java
+++ src/main/java/com/dexter/tong/chapter02/Question01.java
@@ -15,16 +15,46 @@ public class Question01 {
     public static LinkedListNode<Integer> removeDuplicates(LinkedListNode<Integer> head) {
         HashSet<Integer> foundData = new HashSet<>();
 
-        LinkedListNode<Integer> currentNode = head;
-        foundData.add(currentNode.data);
+        LinkedListNode<Integer> current = head;
+        foundData.add(current.data);
 
-        while(currentNode.next != null) {
-            if(foundData.contains(currentNode.next.data)) {
-                currentNode.next = currentNode.next.next;
+        while(current.next != null) {
+            if(foundData.contains(current.next.data)) {
+                current.next = current.next.next;
             } else {
-                foundData.add(currentNode.next.data);
+                foundData.add(current.next.data);
+                current = current.next;
             }
-            currentNode = currentNode.next;
+        }
+
+        return head;
+    }
+
+    /**
+     * 2.1b
+     * Write code to remove duplicates from an unsorted linked list. How would you solve this problem if a temporary buffer is not
+     * allowed?
+     * Time: O(n^2)
+     * Space: O(1)
+     */
+    public static LinkedListNode<Integer> removeDuplicatesNoBuffer(LinkedListNode<Integer> head) {
+        // Maybe there is a way to do a sub-O(n^2) sort here in constant space
+        // Bubble/insertion sort will give O(n^2) runtime
+        // Searching after the ith node for dups will also yield O(n^2) runtime, so I'll do that
+
+        LinkedListNode<Integer> current = head;
+        while(current != null) {
+            LinkedListNode<Integer> before = current;
+            LinkedListNode<Integer> after = before.next;
+            while(after != null) {
+                if(current.data.equals(after.data)) {
+                    before.next = after.next;
+                } else {
+                    before = before.next;
+                }
+                after = before.next;
+            }
+            current = current.next;
         }
 
         return head;
--- src/test/java/com/dexter/tong/chapter02/Question01Test.java
+++ src/test/java/com/dexter/tong/chapter02/Question01Test.java
@@ -4,6 +4,7 @@ import com.dexter.tong.common.LinkedListNode;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -17,5 +18,68 @@ public class Question01Test {
         List<Integer> expected = Arrays.asList(values);
 
         assertEquals(expected, Question01.removeDuplicates(input).asList());
+    }
+
+    @Test
+    public void removeDuplicates_should_remove_one_duplicate_at_end() {
+        Integer[] values = new Integer[]{1, 2, 3, 4, 5, 6, 5};
+        LinkedListNode<Integer> input = utils.createLinkedList(values);
+        List<Integer> expected = new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+
+        assertEquals(expected, Question01.removeDuplicates(input).asList());
+    }
+
+    @Test
+    public void removeDuplicates_should_remove_adjacent_duplicates() {
+        Integer[] values = new Integer[]{6, 9, 3, 2, 2, 2, 1};
+        LinkedListNode<Integer> input = utils.createLinkedList(values);
+        List<Integer> expected = new LinkedList<>(Arrays.asList(6, 9, 3, 2, 1));
+
+        assertEquals(expected, Question01.removeDuplicates(input).asList());
+    }
+
+    @Test
+    public void removeDuplicates_should_remove_duplicates_at_end() {
+        Integer[] values = new Integer[]{6, 9, 3, 2, 1, 6, 6};
+        LinkedListNode<Integer> input = utils.createLinkedList(values);
+        List<Integer> expected = new LinkedList<>(Arrays.asList(6, 9, 3, 2, 1));
+
+        assertEquals(expected, Question01.removeDuplicates(input).asList());
+    }
+
+    @Test
+    public void removeDuplicatesNoBuffer_should_return_identical_if_no_duplicates() {
+        Integer[] values = new Integer[]{1, 7, 3, 5, 9, 6};
+        LinkedListNode<Integer> input = utils.createLinkedList(values);
+        List<Integer> expected = Arrays.asList(values);
+
+        assertEquals(expected, Question01.removeDuplicatesNoBuffer(input).asList());
+    }
+
+    @Test
+    public void removeDuplicatesNoBuffer_should_remove_one_duplicate_at_end() {
+        Integer[] values = new Integer[]{1, 7, 3, 5, 9, 6, 5};
+        LinkedListNode<Integer> input = utils.createLinkedList(values);
+        List<Integer> expected = new LinkedList<>(Arrays.asList(1, 7, 3, 5, 9, 6));
+
+        assertEquals(expected, Question01.removeDuplicatesNoBuffer(input).asList());
+    }
+
+    @Test
+    public void removeDuplicatesNoBuffer_should_remove_adjacent_duplicates() {
+        Integer[] values = new Integer[]{6, 9, 3, 2, 2, 2, 1};
+        LinkedListNode<Integer> input = utils.createLinkedList(values);
+        List<Integer> expected = new LinkedList<>(Arrays.asList(6, 9, 3, 2, 1));
+
+        assertEquals(expected, Question01.removeDuplicatesNoBuffer(input).asList());
+    }
+
+    @Test
+    public void removeDuplicatesNoBuffer_should_remove_duplicates_at_end() {
+        Integer[] values = new Integer[]{6, 9, 3, 2, 1, 6, 6};
+        LinkedListNode<Integer> input = utils.createLinkedList(values);
+        List<Integer> expected = new LinkedList<>(Arrays.asList(6, 9, 3, 2, 1));
+
+        assertEquals(expected, Question01.removeDuplicatesNoBuffer(input).asList());
     }
 }
